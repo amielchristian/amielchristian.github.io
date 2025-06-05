2f1ac3d3ec6c8b58eae680c70a4efb8eaa6d3483
Amiel Christian Mala-ay <amielchristian.malaay.cics@ust.edu.ph>
2025-06-06 02:28:47 +0800

    meta tags for indexing

---
+++ src/components/SEO.svelte
@@ -0,0 +1,4 @@
+<title>Amiel Christian Mala-ay</title>
+<meta name='generator' content={Astro.generator} />
+<meta name="description" content="Welcome! This is the personal website of Amiel Christian Mala-ay, full-stack developer and machine learning practitioner. Here, you can read about me and my works.">
+<meta name="keywords" content="HTML, CSS, JavaScript, TypeScript, Java, Python, machine learning, machine learning practitioner, software developer, full-stack developer, Amiel, Christian, Estrada, Mala-ay, Amiel Christian Estrada Mala-ay, Amiel Christian Mala-ay">
