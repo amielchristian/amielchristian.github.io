e739f94808e5c9ccd9b8c78014d0ed8980927b05
Amiel Christian Mala-ay <amielchristian.malaay.cics@ust.edu.ph>
2025-06-12 02:43:16 +0800

    nav continued

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,9 +1,10 @@
-<nav class='w-full'>
-  <div class='absolute font-serif text-xl font-bold'>Amiel Christian</div>
-  <div class="flex justify-center gap-20">
+<nav class='w-full flex h-20 justify-center items-center p-10'>
+  <div class='font-serif text-xl font-bold'>Amiel Christian</div>
+  <div class="flex justify-center gap-20 place-self-center mx-20">
     <a href="/">Home</a>
     <a href="/about">About</a>
     <a href="/works">Works</a>
     <a href="/contact">Contact</a>
   </div>
+  <button>Night mode!</button>
 </nav>
