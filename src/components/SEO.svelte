c9525d454097c9ab5c26c021269b2ef5c0519494
Amiel Christian Mala-ay <amielchristian.malaay.cics@ust.edu.ph>
2025-06-12 23:20:01 +0800

    add fonts

--- src/components/SEO.svelte
+++
@@ -1,3 +0,0 @@
-<title>Amiel Christian Mala-ay</title>
-<meta name="description" content="Welcome! This is the personal website of Amiel Christian Mala-ay, full-stack developer and machine learning practitioner. Here, you can read about me and my works.">
-<meta name="keywords" content="HTML, CSS, JavaScript, TypeScript, Java, Python, machine learning, machine learning practitioner, software developer, full-stack developer, Amiel, Christian, Estrada, Mala-ay">
--- src/components/global/Nav.svelte
+++ src/components/global/Nav.svelte
@@ -9,13 +9,11 @@
 </script>
 
 <nav
-  id="navbar"
   class="w-full flex flex-row py-2 h-20 justify-between items-center"
 >
-  <div class="w-[15%] font-serif text-xl font-bold">Amiel Christian</div>
+  <div class="w-[15%] font-[Junicode] text-xl font-bold">Amiel Christian</div>
   <div
-    class="hidden sm:flex gap-5 md:gap-20 justify-center place-self-center"
-    id="navlinks"
+    class="hidden sm:flex gap-5 md:gap-20 justify-center place-self-center font-[BricolageGrotesque]"
   >
     <a href="/">Home</a>
     <a href="/about">About</a>
@@ -48,16 +46,16 @@
 </nav>
 <div
   class={`flex flex-col sm:hidden overflow-x-hidden
-  absolute w-full h-full
+  fixed w-full h-full
   transition-[right] duration-150 ease-linear
   bg-white/5 border-t-2
   border-t-[var(--dark)] dark:border-t-[var(--light)]
-  text-[var(--dark)] dark:text-[var(--light)]
+  text-[var(--dark)] dark:text-[var(--light)] text-2xl font-bold font-[BricolageGrotesque]
   backdrop-blur-[18px]
   ${showNav ? 'right-0' : 'right-[-100vw]'}`}
 >
-  <a class='pl-[10%] h-15 border-b-2 flex items-center text-2xl font-bold' href="/">Home</a>
-  <a class='pl-[10%] h-15 border-b-2 flex items-center text-2xl font-bold' href="/about">About</a>
-  <a class='pl-[10%] h-15 border-b-2 flex items-center text-2xl font-bold' href="/works">Works</a>
-  <a class='pl-[10%] h-15 border-b-2 flex items-center text-2xl font-bold' href="/contact">Contact</a>
+  <a class='pl-[10%] h-15 border-b-2 flex items-center' href="/">Home</a>
+  <a class='pl-[10%] h-15 border-b-2 flex items-center' href="/about">About</a>
+  <a class='pl-[10%] h-15 border-b-2 flex items-center' href="/works">Works</a>
+  <a class='pl-[10%] h-15 border-b-2 flex items-center' href="/contact">Contact</a>
 </div>
