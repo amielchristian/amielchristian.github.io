b3b81f9ec80d488eadfc6aed7327af03c5a9d92b
Amiel Christian Mala-ay <amielchristian.malaay.cics@ust.edu.ph>
2025-06-12 15:38:57 +0800

    responsive navlink with slide-in transition

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,36 +4,42 @@
   let showNav = $state(false);
 </script>
 
-<nav id="navbar" class="w-[100%] py-2 h-20 flex justify-between items-center flex-col sm:flex-row">
-  <div class="w-[15%] font-serif text-xl font-bold">Amiel Christian</div>
-  <div
-    class={`${showNav ? 'flex flex-col' : 'hidden'} sm:flex gap-5 md:gap-20 justify-center place-self-center`}
-    id="navlinks"
-  >
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/works">Works</a>
-    <a href="/contact">Contact</a>
-  </div>
-  <div class="w-[15%] flex flex-row gap-4 justify-center">
-    <button
-      class="hover:cursor-pointer"
-      onclick={() => {
-        localStorage.theme = 'light';
-      }}
+<nav id="navbar" class="w-full flex flex-row py-2 h-20 justify-between items-center">
+    <div class="w-[15%] font-serif text-xl font-bold">Amiel Christian</div>
+    <div
+      class='hidden sm:flex gap-5 md:gap-20 justify-center place-self-center'
+      id="navlinks"
     >
-      {#if darkMode}
-        <SunMedium />
-      {:else}
-        <MoonStar />
-      {/if}
-    </button>
-    <button
-      class="hover:cursor-pointer block sm:hidden"
-      onclick={() => {
-        console.log('toggled!');
-        showNav = !showNav;
-      }}><Menu /></button
-    >
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/works">Works</a>
+      <a href="/contact">Contact</a>
+    </div>
+    <div class="w-[15%] flex flex-row gap-4 justify-center">
+      <button
+        class="hover:cursor-pointer"
+        onclick={() => {
+          localStorage.theme = 'light';
+        }}
+      >
+        {#if darkMode}
+          <SunMedium />
+        {:else}
+          <MoonStar />
+        {/if}
+      </button>
+      <button
+        class="hover:cursor-pointer block sm:hidden"
+        onclick={() => {
+          console.log('toggled!');
+          showNav = !showNav;
+        }}><Menu /></button
+      >
   </div>
 </nav>
+<div class={`sm:hidden overflow-x-hidden flex flex-col bg-white absolute w-full h-full duration-150 ease-linear ${showNav ? 'right-0' : 'right-[-100vw]'}`}>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/works">Works</a>
+  <a href="/contact">Contact</a>
+</div>
