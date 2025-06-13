04c38256ca7441f6ab1e70847c375f3357710031
Amiel Christian Mala-ay <amielchristian.malaay.cics@ust.edu.ph>
2025-06-13 23:08:54 +0800

    react man

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -241,11 +241,11 @@ function EmailSVG() {
       xmlSpace='preserve'
       fill='currentColor'
     >
-      <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+      <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
       <g
         id='SVGRepo_tracerCarrier'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       ></g>
       <g id='SVGRepo_iconCarrier'>
         <g>
