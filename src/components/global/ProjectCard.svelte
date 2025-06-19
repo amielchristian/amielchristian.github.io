423e0866d3ecdb3d47d5bf5d3a9ca5f0bd0138d5
Amiel Christian Mala-ay <amielchristian.malaay.cics@ust.edu.ph>
2025-06-19 20:29:54 +0800

    pokemon gradient hover project card

--- src/components/global/ProjectCard.svelte
+++ src/components/global/ProjectCard.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+  // props
+  const { title, image, url, description, index } = $props();
 
-  const { title, image, url, description } = $props();
-  const height = 40;
-  const width = 40;
-  const cellSize = 25;
+  // bindings
   let canvas: HTMLCanvasElement;
+  let canvasContainer: HTMLDivElement;
+  let card: HTMLAnchorElement;
+  let innerWidth = $state(window.innerWidth);
+
+  const colorSchemes: string[][] = [
+    ['#ee9162', '#e1724d', '#f2d045', '#5f7d83', '#356b73', '#ebd69d', '#b1a282', '#26110d'], // cyndaquil
+    ['#DEECD8, #BADBB2, #79AD75, #FBFBFC, #D56671'], // gardevoir
+    ['#425765, #5cc8c7, #fbeda5, #fa6d80, #deeb24'], // dragapult
+    ['#ff3800, #ff4f00, #f9df90, #cb0000, #d87d35'], // fennekin
+    ['#F4D638,#5AB355,#1F766D,#F1F0B8,#9C413E,#F4D638,#5AB355,#1F766D'], //snivy
+    ['#757cbb, #070808, #d2def1, #fbd13d, #a8c3dd'] // chandelure
+  ];
+  const colorScheme: string = colorSchemes[index % colorSchemes.length].join(', ');
 
   function drawGrid(context: CanvasRenderingContext2D) {
+    const height = 40;
+    const width = 40;
+    const cellSize = 25;
     context.beginPath();
     context.strokeStyle = '#ccca';
 
@@ -24,14 +38,26 @@
     context.stroke();
   }
 
-  onMount(() => {
-    // canvas!.height = (cellSize + 1) * height + 1;
-    // canvas!.width = (cellSize + 1) * width + 1;
+  $effect(() => {
+    innerWidth;
+    canvas.width = canvasContainer.offsetWidth;
+    canvas.height = canvasContainer.offsetHeight;
 
     const context: CanvasRenderingContext2D | null = canvas!.getContext('2d');
     if (context) {
       drawGrid(context);
     }
+  });
+
+  $effect(() => {
+    const updateAnimation = () => {
+      const angle =
+        (parseFloat(card.style.getPropertyValue('--angle')) + 0.5) % 360;
+      card.style.setProperty('--angle', `${angle}deg`);
+      requestAnimationFrame(updateAnimation);
+    };
+
+    requestAnimationFrame(updateAnimation);
   });
 </script>
 
@@ -39,18 +65,27 @@
 maybe center after a certain aspect ratio threshold?
 -->
 
+<svelte:window bind:innerWidth />
+
 <a
   href={url}
-  class="relative rounded-md overflow-hidden
+  bind:this={card}
+  style="--angle: 0deg;
+        --border-color: linear-gradient(var(--angle), {colorScheme});"
+  class="project-card relative rounded-md overflow-hidden
         border-2 border-[var(--dark)] hover:border-4
-        animated-border hover:border-transparent
+        [background:padding-box_var(--border-color),border-box_var(--border-color)] hover:border-transparent
         w-full h-75
         group hover:cursor-pointer
-        duration-150"
+        duration-0"
 >
-  <div class="absolute top-0 bg-white dark:bg-black drop-shadow-2xl overflow-hidden w-full h-full">
-    <div class='absolute top-0 w-full h-full bg-linear-to-t from-blue-900 from-10% to-transparent z-3'></div>
-    <canvas class="absolute top-0 z-1 bg-white" width="900" height="300" bind:this={canvas}></canvas>
+  <div
+    class="absolute top-0 bg-white dark:bg-black drop-shadow-2xl overflow-hidden w-full h-full"
+    bind:this={canvasContainer}
+  >
+    <!-- below will be replaced with Fabric, which will be used to allow mouse events -->
+    <canvas class="absolute top-0 z-1 bg-white" width="900" bind:this={canvas}
+    ></canvas>
     <!-- svelte-ignore a11y_missing_attribute -->
     <img
       class="absolute top-0 group-hover:blur-[2px] duration-150 z-2"
