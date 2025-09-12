76c27aecf48ee8e8926aaab0fc64485965bfafee
Amiel Christian Mala-ay <amielchristian.malaay.cics@ust.edu.ph>
2025-09-12 13:35:12 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,12 @@ import svelte from '@astrojs/svelte';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://amielchristian.github.io',
-  integrations: [svelte(), react()],
+  integrations: [svelte(), react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
